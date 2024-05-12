da3d3961bd669441b025b93d542aa70e8adbc38e
kane park <75514808+femmefatalehaein@users.noreply.github.com>
2024-05-13 02:37:21 +0900

    [Bronze I] Title: 유레카 이론, Time: 384 ms, Memory: 21932 KB -BaekjoonHub

---
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+
+class Main{
+	
+	public static boolean isEurekaNumber(int[] triangleNumbers, int K) {
+		for(int i=1; i<=44; i++) {
+			for(int j=1; j<=44; j++) {
+				for(int k=1; k<=44; k++) {
+					int sum = triangleNumbers[i] + triangleNumbers[j] + triangleNumbers[k];
+					if(sum == K) {return true;}
+				}
+			}
+		}
+		return false;
+	}
+
+	
+	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		int triNum[] = new int[45];
+		int triangleNumberCount = 0;
+		for(int i=1; i<=44; i++) {
+			triNum[i] = i*(i+1)/2;
+		}
+		
+		int num[] = new int[T];
+		for(int i=0; i<T; i++) {
+			num[i] = sc.nextInt();
+		}
+	
+		for(int i=0; i<T; i++) {
+			
+			boolean ans = isEurekaNumber(triNum,num[i]);
+			if(ans) {
+				System.out.println(1);
+			}
+			else {
+				System.out.println(0);
+			}
+		}
+			
+	}
+}
